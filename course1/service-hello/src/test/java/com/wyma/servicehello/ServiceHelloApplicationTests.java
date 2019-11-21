d935ae95d57f1a15a8ac9678099eaa8e698004d5
51ma <dingwang@dspsemi.com>
2019-11-21 14:17:20 +0800

    服务注册与发现：eureka

---
+++ course1/service-hello/src/test/java/com/wyma/servicehello/ServiceHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyma.servicehello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
