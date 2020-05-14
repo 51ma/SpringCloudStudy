b206070a8208a9a56f7307de0d00acb3afb9da10
51ma <51ma@qq.com>
2020-05-14 10:18:40 +0800

    Config（高可用分布式配置中心）

---
+++ course7/client-config/src/test/java/com/wyma/config/clientconfig/ClientConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyma.config.clientconfig;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ course7/server-config/src/test/java/com/wyma/config/serverconfig/ServerConfigApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wyma.config.serverconfig;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
